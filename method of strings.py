fc7ab0a9ea51fc86f259d4d3ca88da7b61377997
majideveloper <majidmoradi1368m68@gmail.com>
2023-04-15 01:37:06 +0430

    strings method

---
+++ method of strings.py
@@ -0,0 +1,22 @@
+# len()
+name = "mAjidmoradi"
+print(len(name))
+print(name[ : len(name)])#az ebteda ta yeki ghabl az kol tool len chap mikonad
+
+# ---------------
+print(name.upper())
+print(name.lower())
+print(name.count("m"))
+print(name.endswith("m"))
+print(name.startswith("m"))
+print(name.find("m"))
+print(name.rfind("m"))
+print(name.isalnum())
+x = "*"
+y = ['a','b','c']
+print(x.join(y))
+x = "Majid , ali , saeed , 100"
+
+print(x.split(','))
+print((ord('A')))
+print("char:  \u00F1")
--- pow.py
+++ pow.py
@@ -1,3 +1,7 @@
 x = float(input("Please Enter Number1: "))
 y = float(input("Please Enter Number2: "))
-print("Tavane  is:", pow(x,y))
+z = float(input("Please Enter Number3: "))
+m = (x + y + z)/3
+print("Average is:",m)
+import math
+print(sum(x,y,z )/3)
