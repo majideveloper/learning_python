38ddaed97fa9fe0d8532b810a4f26ec5e69250f8
majideveloper <majidmoradi1368m68@gmail.com>
2023-04-14 16:12:01 +0430

    Mohit

---
+++ Mohit va masahat.py
@@ -0,0 +1,7 @@
+# Mohit = ghotr * 3.14
+# Masahat= shoaa * shoaa * 3.14
+x = float(input("Please Enter a radius : "))
+mo = x * 2 * 3.14
+print("Mohit is=",mo)
+ma = x * x * 3.14
+print("Masahat is=", ma)
