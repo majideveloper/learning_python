16cf3c7ee7ea851027b65e3f251b978e5e9faccb
majideveloper <majidmoradi1368m68@gmail.com>
2023-04-15 21:34:11 +0430

    Special character

---
+++ How to use %.py
@@ -0,0 +1,12 @@
+x = 3
+y = 2.621321313
+s = "reza"
+print("x is: %i\ny is: %i\nz is: %i "%( x ,y , 120.5) )
+# [(key)] [flag] [w] [.p]
+print("%i"%(5))
+print("%c"%(110))#baraye namayesh charactere code ascii
+print("%s"%(s))#baraye namayeshe yek reshte
+print("%8.3f"%(y))#[w],[.p]
+a1 = int(input("Please enter weight:"))
+a2 = int(input("Please enter point of float:"))
+print("%*.*f" %(a1,a2,y))
